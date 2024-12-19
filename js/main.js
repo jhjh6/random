7a28623d9cd13c6940a6a4b8042edd07577847fa
jhjh6 <187578565+jhjh6@users.noreply.github.com>
2024-12-19 14:05:59 +0900

    add info

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const result = document.getElementById('result');
 const history = document.getElementById('history');
 const historyTitle = document.getElementById('history-title');
 const clearHistoryBtn = document.getElementById('clear-history');
+const infoBtn = document.getElementById('info-btn');
+const infoPopup = document.getElementById('info-popup');
+const closeBtn = document.querySelector('.close-btn');
 
 btn.addEventListener('click', () =>{
     const num = Math.floor(Math.random() * (max.value - min.value + 1)) + Number(min.value);
@@ -27,4 +30,18 @@ clearHistoryBtn.addEventListener('click', () => {
     history.innerHTML = '';
     historyTitle.style.display = 'none';
     clearHistoryBtn.style.display = 'none';
+});
+
+infoBtn.addEventListener('click', () => {
+    infoPopup.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    infoPopup.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === infoPopup) {
+        infoPopup.style.display = 'none';
+    }
 });
